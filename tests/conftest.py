83b349b99fb116773d04782d2e6477d491c5cedd
mobal <bmolnar0510@gmail.com>
2022-08-02 16:09:46 +0200

    Added missing environment variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,8 @@ def set_environment_variables(monkeypatch):
     monkeypatch.setenv('APP_NAME', 'auth-service')
     monkeypatch.setenv('APP_STAGE', 'test')
     monkeypatch.setenv('APP_TIMEZONE', 'Europe/Budapest')
+    monkeypatch.setenv('JWT_SECRET', 'p2s5v8y/B?E(H+MbPeShVmYq3t6w9z$C')
+    monkeypatch.setenv('CACHE_SERVICE_BASE_URL', 'https://localhost')
 
 
 @pytest.fixture
