1ad2b9a41a1c46cd1e408ec4136d049acb49e7e5
Balázs Molnár <bmolnar0510@gmail.com>
2024-08-12 14:23:24 +0200

    fix: replaced app timezone with default timezone

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str
-    app_timezone: str
+    default_timezone: str
     aws_access_key_id: str
     aws_secret_access_key: str
     jwt_token_lifetime: int = 3600
--- lambda.tf
+++ lambda.tf
@@ -39,16 +39,16 @@ resource "aws_lambda_function" "fastapi" {
   environment {
     variables = {
       APP_NAME                             = var.app_name
-      APP_TIMEZONE                         = var.default_timezone
+      CACHE_SERVICE_API_KEY_SSM_PARAM_NAME = var.cache_service_api_key_ssm_param_name
       CACHE_SERVICE_BASE_URL               = var.cache_service_base_url
       DEBUG                                = var.debug
+      DEFAULT_TIMEZONE                     = var.default_timezone
       JWT_SECRET_SSM_PARAM_NAME            = var.jwt_secret_ssm_param_name
       LOG_LEVEL                            = var.log_level
       POWERTOOLS_LOGGER_LOG_EVENT          = "true"
       POWERTOOLS_SERVICE_NAME              = var.power_tools_service_name
       POWERTOOLS_DEBUG                     = "false"
       STAGE                                = var.stage
-      CACHE_SERVICE_API_KEY_SSM_PARAM_NAME = var.cache_service_api_key_ssm_param_name
     }
   }
 
