cacc6eebf2611fd8c7c17b60ddea4b4616c80f94
Balázs Molnár <bmolnar0510@gmail.com>
2025-02-24 20:40:38 +0100

    chore: added time-machine and updated dependencies

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,11 +1,18 @@
+from typing import Any
+
 from fastapi import HTTPException, status
 
 
 class CacheServiceException(HTTPException):
-    def __init__(self, detail):
+    def __init__(self, detail: Any):
         super().__init__(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail
         )
+
+
+class ExpiredRefreshTokenException(HTTPException):
+    def __init__(self, detail: Any):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
 
 
 class NotFoundException(HTTPException):
@@ -15,6 +22,13 @@ class NotFoundException(HTTPException):
 
 class UserNotFoundException(NotFoundException):
     pass
+
+
+class TokenMistmatchException(HTTPException):
+    def __init__(self, detail: Any):
+        super().__init__(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail
+        )
 
 
 class TokenNotFoundException(NotFoundException):
