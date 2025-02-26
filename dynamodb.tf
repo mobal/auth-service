b66888496ffbac4a85ef08651836dc9afbd14655
Balázs Molnár <bmolnar0510@gmail.com>
2025-02-26 18:08:24 +0100

    feat: added tokens table

--- dynamodb.tf
+++ dynamodb.tf
@@ -20,3 +20,29 @@ resource "aws_dynamodb_table" "users" {
   }
 
 }
+
+resource "aws_dynamodb_table" "tokens" {
+  name         = "${var.stage}-tokens"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "jti"
+
+  attribute {
+    name = "jti"
+    type = "S"
+  }
+
+  attribute {
+    name = "jwt_token"
+    type = "S"
+  }
+
+  attribute {
+    name = "refresh_token"
+    type = "S"
+  }
+
+  attribute {
+    name = "ttl"
+    type = "N"
+  }
+}
