5786dc1c6aac0805062c7afc0362a7c44b793b64
Balázs Molnár <797870+mobal@users.noreply.github.com>
2025-12-01 10:26:07 +0100

    fix: logging

--- app/jwt_bearer.py
+++ app/jwt_bearer.py
@@ -39,7 +39,7 @@ class HTTPBearer(FastAPIHTTPBearer):
     ) -> HTTPAuthorizationCredentials | None:
         scheme, credentials = get_authorization_scheme_param(authorization)
         if not (authorization and scheme and credentials):
-            logger.warning(f"Missing {authorization=}, {scheme=} or {credentials=}")
+            logger.warning("Missing authorization, scheme or credentials")
             if self._auto_error:
                 raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN,
