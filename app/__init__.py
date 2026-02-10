9cda7369dc648afa4a3f3ca709a8b483dcdeab74
Balázs Molnár <797870+mobal@users.noreply.github.com>
2026-02-10 21:52:08 +0100

    refactor: format with ruff

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,8 @@ from dotenv import load_dotenv
 
 from app.settings import Settings
 
-env_files = [
-    ".env",
-    ".env.dev",
-    ".env.local",
-    ".env.prod"
-]
+env_files = [".env", ".env.dev", ".env.local", ".env.prod"]
+
 
 def load_env_files():
     root_dir = Path(__file__).parent.parent
@@ -17,6 +13,7 @@ def load_env_files():
         f = root_dir / env
         if f.exists():
             load_dotenv(dotenv_path=f, override=False)
+
 
 load_env_files()
 
