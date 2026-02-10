7fbb0f390f145b3b4b8106864ebb6e7e6d737561
Balázs Molnár <797870+mobal@users.noreply.github.com>
2026-02-10 20:34:15 +0100

    feat: add support for multiple .env files

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 from app.settings import Settings
 
-load_dotenv()
+PROJECT_DIR = Path(__file__).parent.parent
+
+env_files = [".env", ".env.dev", ".env.local", ".env.prod"]
+
+for env in env_files:
+    f = PROJECT_DIR / env
+    if f.exists():
+        load_dotenv(dotenv_path=f, override=True)
 
 settings = Settings()
