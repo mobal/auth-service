479f9ca5ac5182aed93ff6e54df52e6ef8cc7f69
Balázs Molnár <797870+mobal@users.noreply.github.com>
2026-02-10 21:50:02 +0100

    refactor: do not override previously loaded env vars

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,20 @@ from dotenv import load_dotenv
 
 from app.settings import Settings
 
-PROJECT_DIR = Path(__file__).parent.parent
+env_files = [
+    ".env",
+    ".env.dev",
+    ".env.local",
+    ".env.prod"
+]
 
-env_files = [".env", ".env.dev", ".env.local", ".env.prod"]
+def load_env_files():
+    root_dir = Path(__file__).parent.parent
+    for env in env_files:
+        f = root_dir / env
+        if f.exists():
+            load_dotenv(dotenv_path=f, override=False)
 
-for env in env_files:
-    f = PROJECT_DIR / env
-    if f.exists():
-        load_dotenv(dotenv_path=f, override=True)
+load_env_files()
 
 settings = Settings()
