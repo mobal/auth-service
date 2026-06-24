47981d56418d7f90c6249016f450abb1405fbc45
Balázs Molnár <bmolnar0510@gmail.com>
2026-06-24 21:28:44 +0200

    fix: add ConditionExpression and ConsistentRead to service repository

--- app/repositories/service_repository.py
+++ app/repositories/service_repository.py
@@ -1,6 +1,6 @@
 import boto3
 from aws_lambda_powertools import Logger
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Attr, Key
 
 from app import settings
 from app.models.service import ServiceCredential
@@ -15,12 +15,15 @@ class ServiceRepository:
 
     def create_service(self, data: dict) -> dict:
         self._logger.info("Creating service credential record")
-        return self._table.put_item(Item=data)
+        return self._table.put_item(
+            Item=data,
+            ConditionExpression=Attr("id").not_exists(),
+        )
 
     def get_by_id(self, client_id: str) -> ServiceCredential | None:
         self._logger.debug("Fetching service credential by client_id=%s", client_id)
 
-        response = self._table.get_item(Key={"id": client_id})
+        response = self._table.get_item(Key={"id": client_id}, ConsistentRead=True)
         if "Item" in response:
             return ServiceCredential(**response["Item"])
         self._logger.warning("Service credential not found for client_id=%s", client_id)
